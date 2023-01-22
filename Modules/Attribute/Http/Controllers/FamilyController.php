8b0eb5457aed7d9f223759bfaf98f9fb4fb0b199
vitor <you@example.com>
2023-01-22 12:35:48 -0300

    ajuste attribute module

--- Modules/Attribute/Http/Controllers/FamilyController.php
+++ Modules/Attribute/Http/Controllers/FamilyController.php
@@ -1,33 +1,86 @@
 <?php
 
 namespace Modules\Attribute\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Validator;
+use Modules\Attribute\Services\Attribute\FamilyService;
 
 class FamilyController
 extends Controller
 {
-    public function index()
+    public function index(FamilyService $service)
     {
-
+        $response = $service->index();
+        if ($response instanceof \Illuminate\Http\JsonResponse) {
+            return $response;
+        }
+        ResourceCollection::$wrap = 'attribute_families';
+        return ResourceCollection::make($response);
     }
 
-    public function store()
+    public function store(FamilyService $service, Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|required|min:2|max:30|unique:attribute_families,name'
+        ], [
+            'string' => __('attribute::general.family.name.string'),
+            'required' => __('attribute::general.family.name.required'),
+            'min' => __('attribute::general.family.name.min'),
+            'max' => __('attribute::general.family.name.max'),
+            'unique' => __('attribute::general.family.name.unique')
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(
+                $validator->errors(),
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $data = $validator->validated();
+        $response = $service->store($data);
+        if ($response instanceof \Illuminate\Http\JsonResponse) {
+            return $response;
+        }
+        return response()->json([
+            'message' => __('attribute::general.family.store.success'),
+            'attribute_family' => $response->toArray()
+        ], Response::HTTP_OK);
     }
 
-    public function show(int $id)
+    public function show(FamilyService $service, int $id)
     {
-
+        return response()->json([
+            'attribute_family' => $service->show($id)
+        ], Response::HTTP_OK);
     }
 
-    public function update(int $id)
+    public function update(FamilyService $service, Request $request, int $id)
     {
+        $data = $request->validated();
 
+        $response = $service->update($id, $data);
+        if ($response instanceof \Illuminate\Http\JsonResponse) {
+            return $response;
+        }
+        return response()->json([
+            'message' => __('attribute::general.family.update.success'),
+            'attribute_family' => $response->toArray()
+        ]);
     }
 
-    public function destroy(int $id)
+    public function destroy(FamilyService $service, int $id)
     {
-
+        $response = $service->destroy($id);
+        if ($response instanceof \Illuminate\Http\JsonResponse) {
+            return $response;
+        }
+        return response()->json([
+            'message' => __('attribute::general.family.destroy.success')
+        ], Response::HTTP_OK);
     }
 }
--- Modules/Attribute/Providers/AttributeServiceProvider.php
+++ Modules/Attribute/Providers/AttributeServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Modules\Attribute\Providers;
 
+use Modules\Attribute\Entity\Family;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Modules\Attribute\Services\Attribute\FamilyService;
 
 class AttributeServiceProvider
 extends ServiceProvider
@@ -17,8 +20,24 @@ extends ServiceProvider
      */
     protected $moduleNameLower = 'attribute';
 
+    public function boot()
+    {
+        $this->dependencyInjection();
+    }
+
     public function register()
     {
         $this->app->register(RouteSerivceProvider::class);
+    }
+
+    /**
+     * Aplica as injeções de dependecias de acordo com as 
+     * necessidades do modulo
+     */
+    public function dependencyInjection()
+    {
+        $this->app->when(FamilyService::class)
+            ->needs(Model::class)
+            ->give(Family::class);
     }
 }
