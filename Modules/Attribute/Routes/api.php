fb574776d25eab6911da936ecfb55f4a288c9ce6
vitor <you@example.com>
2023-01-22 18:41:13 -0300

    rename urls api - attribute

--- Modules/Attribute/Routes/api.php
+++ Modules/Attribute/Routes/api.php
@@ -3,5 +3,6 @@
 Route::prefix('attribute')->group(function () {
     Route::apiResource('families', 'FamilyController')->parameters(['families' =>'id']);
     Route::apiResource('groups', 'GroupController')->parameters(['groups' => 'id']);
-    Route::apiResource('', 'AttributeController')->parameters(['' => 'id']);
 });
+
+Route::apiResource('attributes', 'AttributeController')->parameters(['attributes' => 'id']);
