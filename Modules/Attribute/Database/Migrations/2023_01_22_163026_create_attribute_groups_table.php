e6396cc2f6879c2ad79c1f153f92eb15614b2664
vitor <you@example.com>
2023-01-22 13:33:20 -0300

    migrate attribute groups

---
+++ Modules/Attribute/Database/Migrations/2023_01_22_163026_create_attribute_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->unsignedBigInteger('attribute_family_id');
+            $table->foreign('attribute_family_id')
+                ->references('id')
+                ->on('attribute_families')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_groups');
+    }
+}
